38f1a2f86bb3b9e8c74afbc82081f63848720984
Grant Firl <grantf@ucar.edu>
2019-12-27 10:12:45 -0700

    change GSD suite to v1 in multi_run configure file for modifications

--- scm/src/short_course_runs2.py
+++ scm/src/short_course_runs2.py
@@ -1,3 +1,3 @@
 cases = ["arm_sgp_summer_1997_A"]
-suites = ["SCM_GFS_v15p2","SCM_GSD_v0","SCM_csawmg","SCM_csawmg"]
-namelists = ["input_GFS_v15p2.nml","input_GSD_v0.nml","input_csawmg.nml","input_csawmg_short_course.nml"]
+suites = ["SCM_GFS_v15p2","SCM_GSD_v1","SCM_csawmg","SCM_csawmg"]
+namelists = ["input_GFS_v15p2.nml","input_GSD_v1.nml","input_csawmg.nml","input_csawmg_short_course.nml"]
